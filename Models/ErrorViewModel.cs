a05a8c832cdd2705cde07d94bf5d164cd5e7fe3e
Pahan200-7 <pavlik.yakovlev.2000makron@mail.ru>
2025-08-06 18:07:34 +0300

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pozdravlyator.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
